601a4dea7ceca2cc4175dbe68bd7544a3668cea1
HasaraSankalpaDev <hasarasankalpa176@gmail.com>
2024-10-23 20:49:27 +0530

    Compleated Food Item Adding Part

--- app/Admin/AddFood/page.jsx
+++ app/Admin/AddFood/page.jsx
@@ -1,6 +1,57 @@
-import React from "react";
+"use client";
+import React, { useState } from "react";
+import axios from "axios";
+import { toast } from "react-toastify";
 
-const page = () => {
+const Page = () => {
+  // State Handle
+  const [image, setImage] = useState(null);
+  const [data, setData] = useState({
+    itemName: "",
+    itemPrice: "",
+    itemDescription: "",
+    itemCategory: "",
+    itemImage: "",
+  });
+
+  const onChangeHandler = (event) => {
+    const { name, value } = event.target;
+    setData((prevData) => ({ ...prevData, [name]: value }));
+  };
+
+  // Image Handle
+  const onFileChange = (e) => {
+    if (e.target.files && e.target.files[0]) {
+      setImage(e.target.files[0]);
+    }
+  };
+
+  // Submit Handler
+  const onSubmitHandler = async (e) => {
+    e.preventDefault();
+
+    const formData = new FormData();
+    formData.append("itemName", data.itemName);
+    formData.append("itemPrice", data.itemPrice);
+    formData.append("itemDescription", data.itemDescription);
+    formData.append("itemCategory", data.itemCategory);
+    formData.append("itemImage", image);
+
+    // Sending API
+    const response = await axios.post(
+      "http://localhost:3000/Api/food",
+      formData
+    );
+
+    console.log(response);
+
+    if (response.data.success) {
+      toast.success("Food Item Saved Succesfully !");
+    } else {
+      toast.success(response.data.msg);
+    }
+  };
+
   return (
     <div>
       <div className="mt-4">
@@ -11,63 +62,76 @@ const page = () => {
         </p>
       </div>
       <div className="w-1/2 mt-5">
-        <form>
+        <form onSubmit={onSubmitHandler}>
           <div className="my-4">
-            <label htmlFor="">Item Name</label>
+            <label htmlFor="itemName">Item Name</label>
             <input
               type="text"
+              name="itemName"
               className="bg-gray-100 py-3 px-4 mt-3 w-full"
               placeholder="Enter Food Item Name"
+              onChange={onChangeHandler}
+              value={data.itemName}
             />
           </div>
           <div className="my-4">
-            <label htmlFor="">Item Description</label>
-
+            <label htmlFor="itemDescription">Item Description</label>
             <textarea
               rows={3}
-              type="text"
+              name="itemDescription"
               className="bg-gray-100 py-3 px-4 mt-3 w-full border-none"
               placeholder="Enter Food Item Description"
+              onChange={onChangeHandler}
+              value={data.itemDescription}
             />
           </div>
           <div className="my-4">
-            <label htmlFor="">Item Price</label>
-
+            <label htmlFor="itemPrice">Item Price</label>
             <div className="flex items-center bg-gray-100 mt-3 px-4 w-full">
-              <span className="text-gray-700 mr-2">$</span>{" "}
-              {/* Currency operator */}
+              <span className="text-gray-700 mr-2">$</span>
               <input
                 type="number"
+                name="itemPrice"
                 className="bg-gray-100 flex-grow py-3 px-4 w-full border-none"
                 placeholder="Enter Price"
                 min="0"
                 step="0.01"
+                onChange={onChangeHandler}
+                value={data.itemPrice}
               />
             </div>
           </div>
           <div className="my-4">
-            <label htmlFor="">Item Image</label>
-
+            <label htmlFor="itemImage">Item Image</label>
             <input
               type="file"
+              name="itemImage"
               className="py-3 px-4 bg-gray-100  w-full mt-3"
-              placeholder="Food Item Image  "
+              onChange={onFileChange}
             />
           </div>
-          <label htmlFor="">Item Category</label>
+          <label htmlFor="itemCategory">Item Category</label>
           <div className="">
-            <select className=" mt-4 px-4 py-3 border bg-gray-100 w-1/2">
+            <select
+              name="itemCategory"
+              className="mt-4 px-4 py-3 border bg-gray-100 w-1/2"
+              onChange={onChangeHandler}
+              value={data.itemCategory}
+            >
               <option value="Salads">Salads</option>
               <option value="Desserts">Desserts</option>
               <option value="Sandwiches">Sandwiches</option>
               <option value="Cakes">Cakes</option>
-              <option value="Juce">Juce</option>
+              <option value="Juice">Juice</option>
               <option value="Noodles">Noodles</option>
             </select>
           </div>
 
           <div className="mt-6">
-            <button className="w-full py-3 px-4 bg-black text-white hover:shadow-lg">
+            <button
+              type="submit"
+              className="w-full py-3 px-4 bg-black text-white hover:shadow-lg"
+            >
               Add Food Item
             </button>
           </div>
@@ -77,4 +141,4 @@ const page = () => {
   );
 };
 
-export default page;
+export default Page;
--- app/Api/food/route.js
+++ app/Api/food/route.js
@@ -9,64 +9,62 @@ import { connectDB } from "@/Components/Lib/Config/Db.config";
 // Foods Listing Api Endpoint
 
 export async function GET(request) {
-    const { searchParams } = new URL(request.url);
-    const foodId = searchParams.get("id");
-  
-    if (foodId) {
-      const food = await foodModel.findById(foodId);
-      return NextResponse.json({ food });
-    } else {
-      const foods = await foodModel.find({});
-      return NextResponse.json({ msg: "Foods Found", foods });
-    }
+  const { searchParams } = new URL(request.url);
+  const foodId = searchParams.get("id");
+
+  if (foodId) {
+    const food = await foodModel.findById(foodId);
+    return NextResponse.json({ food });
+  } else {
+    const foods = await foodModel.find({});
+    return NextResponse.json({ msg: "Foods Found", foods });
   }
-  
+}
 
 // Food Saving Api Endpoint
 export async function POST(request) {
+  await connectDB();
+  const foodData = await request.formData();
+  const timestamp = Date.now();
 
-    await connectDB()
-    const foodData = await request.formData();
-    const timestamp = Date.now();
-  
-    // Get the image file
-    const image = foodData.get("itemImage");
-  
-    if (image && typeof image.arrayBuffer === "function") {
-      // Convert image to arrayBuffer
-      const imageByteData = await image.arrayBuffer();
-  
-      // Create a buffer from the image data
-      const buffer = Buffer.from(imageByteData);
-  
-      // Define path for saving the image
-      const path = `./public/${timestamp}_${image.name}`;
-  
-      // Write the image to the filesystem
-      await writeFile(path, buffer);
-  
-      // Define the image URL path
-      const imgUrl = `/${timestamp}_${image.name}`;
-  
-      // Construct the food data
-      const itemData = {
-        itemName: `${foodData.get("itemName")}`,
-        itemPrice: `${foodData.get("itemPrice")}`,
-        itemDescription: `${foodData.get("itemDescription")}`,
-        itemCategory: `${foodData.get("itemCategory")}`,
-        itemImage: `${imgUrl}`,
-      };
-  
-      // Save the food data to the database
-      await foodModel.create(itemData);
-      console.log("Food Created");
-  
-      return NextResponse.json({
-        success: true,
-        msg: "Food Saved",
-        imageUrl: imgUrl,
-      });
-    } else {
-      return NextResponse.json({ success: false, msg: "Invalid image format" });
-    }
+  // Get the image file
+  const image = foodData.get("itemImage");
+
+  if (image && typeof image.arrayBuffer === "function") {
+    // Convert image to arrayBuffer
+    const imageByteData = await image.arrayBuffer();
+
+    // Create a buffer from the image data
+    const buffer = Buffer.from(imageByteData);
+
+    // Define path for saving the image
+    const path = `./public/${timestamp}_${image.name}`;
+
+    // Write the image to the filesystem
+    await writeFile(path, buffer);
+
+    // Define the image URL path
+    const imgUrl = `/${timestamp}_${image.name}`;
+
+    // Construct the food data
+    const itemData = {
+      itemName: `${foodData.get("itemName")}`,
+      itemPrice: `${foodData.get("itemPrice")}`,
+      itemDescription: `${foodData.get("itemDescription")}`,
+      itemCategory: `${foodData.get("itemCategory")}`,
+      itemImage: `${imgUrl}`,
+    };
+
+    // Save the food data to the database
+    await foodModel.create(itemData);
+    console.log("Food Saved Succesfully");
+
+    return NextResponse.json({
+      success: true,
+      msg: "Food Saved",
+      imageUrl: imgUrl,
+    });
+  } else {
+    return NextResponse.json({ success: false, msg: "Invalid image format" });
   }
+}
