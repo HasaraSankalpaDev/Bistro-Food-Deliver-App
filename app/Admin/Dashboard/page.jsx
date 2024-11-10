beef55f0ee7eccfa4d2a7ed1417102ea3513b920
HasaraSankalpaDev <hasarasankalpa176@gmail.com>
2024-11-10 21:30:05 +0530

    Fix Admin Dashboard Error

--- app/Admin/Dashboard/page.jsx
+++ app/Admin/Dashboard/page.jsx
@@ -1,16 +1,9 @@
 "use client";
 import React, { useEffect, useState } from "react";
 import UserProfile from "@/Components/Components/Admin/AdminProfile";
-import { useRouter } from "next/navigation";
 
 function page() {
   const [adminId, setAdminId] = useState();
-  const router = useRouter();
-
-  // Check user is real user
-  if (!adminId) {
-    router.push("http://localhost:3000/pages/404");
-  }
 
   useEffect(() => {
     const id = localStorage.getItem("adminId");
